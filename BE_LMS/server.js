9b2871c882ff853fb0bf40a99c8ff6d2aede2111
quangcoderrq <nhanthiengg123@gmail.com>
2026-06-11 08:40:20 +0700

    fix typescript version

--- BE_LMS/server.js
+++ BE_LMS/server.js
@@ -1,7 +1,7 @@
 const io = new Server(server, {
   cors: {
     origin: [
-      "https://ten-app-frontend.vercel.app",   // URL frontend sau này
+      "https://lms-project-beta-two.vercel.app/",   // URL frontend sau này
       "http://localhost:3000"                 // để test local
     ],
     methods: ["GET", "POST"],
